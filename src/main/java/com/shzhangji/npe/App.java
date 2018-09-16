983195ccbf7edf391873184f40d8e591cc0d0203
Ji ZHANG <zhangji87@gmail.com>
2018-09-16 08:19:21 +0800

    init

---
+++ src/main/java/com/shzhangji/npe/App.java
@@ -0,0 +1,13 @@
+package com.shzhangji.npe;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
